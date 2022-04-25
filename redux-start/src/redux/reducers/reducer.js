f62fe7dfce17f097fc2b90c4b24feb4be2e465cf
MOON <sunpositive7@gmail.com>
2022-04-25 20:22:42 +0900

    combineReducers 실습(combine으로 대표 리듀서를 생성)

---
+++ redux-start/src/redux/reducers/reducer.js
@@ -0,0 +1,10 @@
+import { combineReducers } from "redux";
+import filter from "./filter";
+import todos from "./todos";
+
+const reducer = combineReducers({
+    todos,
+    filter
+});
+
+export default reducer;
